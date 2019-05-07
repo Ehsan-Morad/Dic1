969777cb7cd48cb54fed9a23bc313ce7b33d36b0
Your Name <you@example.com>
2019-05-08 01:39:29 +0430

    file edited

--- dic1.php
+++ dic1.php
@@ -46,10 +46,16 @@ use PHPHtmlParser\Dom;
 	 
 	if ($number1> 0 or $number2>0 )
 	{
-		$main_mean="لغت مورد نظر یافت نشد";
-		echo'<!DOCTYPE html><html><head></head><body dir=rtl>';
-		echo ("<h1  style=color:red;margin-right:100px;>$main_mean</h1>");
-		echo '</body></html>';
+		
+		//echo'<!DOCTYPE html><html><head></head><body dir=rtl>';
+		//echo ("<h1  style=color:red;margin-right:100px;>$main_mean</h1>");
+		//echo '</body></html>';
+		$Result["Result"]="لغت مورد نظر یافت نشد";
+		echo preg_replace_callback("/\\\\u([a-f0-9]{4})/iu", function($m){return iconv('UCS-4LE','UTF-8',pack('V', hexdec('U'.$m[1])));}, json_encode($Result));
+		//$dic_res->result="لغت مورد نظر یافت نشد";
+		//$myJSON = json_encode($dic_res);
+
+		//echo $myJSON;
 		
 	}
 	elseif ($number3>0)
@@ -57,18 +63,44 @@ use PHPHtmlParser\Dom;
 		$contents3 = $dom->find('.js-definitions');
 		$main_mean=$contents3->find('.js-definitions');
 		$main_mean=$contents3->text;
-		$contents3 = $contents3->find('.result__sentences');
-		echo'<!DOCTYPE html><html><head></head><body dir=rtl>';
-		echo ("<h1  style=color:red;margin-right:100px;>$main_mean</h1>");
+		$contents3 = $contents3->find('.result__sentences li');
+		//$contents3 = $contents3->find('li');
+		//echo'<!DOCTYPE html><html><head></head><body dir=rtl>';
+		//echo ("<h1  style=color:red;margin-right:100px;>$main_mean</h1>");
 
 		foreach ($contents3 as $key => $value) {
-
-
-			echo "<h3  lstyle=color:blue;text-align:center>$value</h3>";
+				
+				$res[]= $value->text;
+				
+				
+				//$id=0;
+			//echo "<h3  lstyle=color:blue;text-align:center>$value</h3>";
+			//$res[]=$value;
+			//$dic_res->result=$value->text;
+			//$id++;
+			//echo $value;
+			//echo $contents3;
 
 		}
+		foreach ($res as $key=>$value)
 		
-		echo '</body></html>';
+		{
+			
+			if (fmod($key, 2)==0)
+			{
+				
+				$Result["Result"][]=array($res[$key]=>$res[$key+1]);
+			}
+			
+		} 
+		
+		//echo var_dump($Result);
+		 //$res=implode(" ",$res);
+		//echo $res;
+		echo preg_replace_callback("/\\\\u([a-f0-9]{4})/iu", function($m){return iconv('UCS-4LE','UTF-8',pack('V', hexdec('U'.$m[1])));}, json_encode($Result));
+		//$myJSON = json_encode($Result);
+		//echo $myJSON;
+		
 		
 		
 	}elseif($number4>0)
